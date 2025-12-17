57579450e1634ed35174313acdd5758877f932de
Ibrahim <khanibrahim7208@gmail.com>
2025-12-17 09:38:27 +0530

    Fix-Few commit

--- backend/middleware/isLoggedInMiddleware.js
+++ backend/middleware/isLoggedInMiddleware.js
@@ -1,7 +1,4 @@
 import jwt from "jsonwebtoken"
-import cookieParser from "cookie-parser";
-
-app.use(cookieParser());
 
 export const isLoggedIn = (req, res, next)=>{
     let token = req.cookies.token
